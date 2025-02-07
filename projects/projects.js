ec026df119c69407952a337ac7cdb8f5c36d8f0e
alexfrederick1 <afrederick@ucsd.edu>
2025-02-07 14:32:07 -0800

    devers

--- projects/projects.js
+++ projects/projects.js
@@ -9,20 +9,46 @@ projectsTitle.textContent = `${projects.length} Projects`;
 
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
-let data = [1, 2, 3, 4, 5, 5];
+let data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' },
+];
 
-let sliceGenerator = d3.pie();
+let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
 
 let arcs = arcData.map((d) => arcGenerator(d));
 
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
+// Create container for pie chart and legend
+let container = d3.select('.projects').append('div').attr('class', 'container');
+
+// Append pie chart (SVG)
+let svg = container.append('svg').attr('viewBox', '-50 -50 100 100');
+
 arcs.forEach((arc, idx) => {
-  d3.select('svg')
-    .append('path')
+  svg.append('path')
     .attr('d', arc)
     .attr('fill', colors(idx));
 });
+
+// Create legend
+let legend = container.append('ul').attr('class', 'legend');
+
+legend.selectAll('li')
+  .data(data)
+  .enter()
+  .append('li')
+  .html((d, idx) => {
+    return `
+      <span class="swatch" style="background-color: ${colors(idx)};"></span>
+      ${d.label} <em>(${d.value})</em>
+    `;
+  });
 
 renderProjects(projects, projectsContainer, 'h2');
