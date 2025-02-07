91cfd188147ec6118d34215c7674e6fc660813d6
alexfrederick1 <afrederick@ucsd.edu>
2025-02-07 14:36:29 -0800

    n

--- projects/projects.js
+++ projects/projects.js
@@ -9,9 +9,16 @@ projectsTitle.textContent = `${projects.length} Projects`;
 
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
-let data = [1, 2, 3, 4, 5, 5];
+let data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' },
+];
 
-let sliceGenerator = d3.pie();
+let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
 
 let arcs = arcData.map((d) => arcGenerator(d));
@@ -24,5 +31,18 @@ arcs.forEach((arc, idx) => {
     .attr('d', arc)
     .attr('fill', colors(idx));
 });
+
+let legend = d3.select('.projects').append('ul').attr('class', 'legend');
+
+legend.selectAll('li')
+  .data(data)
+  .enter()
+  .append('li')
+  .html((d, idx) => {
+    return `
+      <span class="swatch" style="background-color: ${colors(idx)};"></span>
+      ${d.label} <em>(${d.value})</em>
+    `;
+  });
 
 renderProjects(projects, projectsContainer, 'h2');
