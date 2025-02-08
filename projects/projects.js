992785b4404ba0af1fc817b8394b3233e34e6445
alexfrederick1 <afrederick@ucsd.edu>
2025-02-07 20:28:28 -0800

    a

--- projects/projects.js
+++ projects/projects.js
@@ -3,14 +3,14 @@ import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
+const projectTitle = document.querySelector('.projects-title'); // Update this selector for project title
 
-const projectsTitle = document.querySelector('.projects-title');
-projectsTitle.textContent = `${projects.length} Projects`;
+projectTitle.innerHTML = `${projects.length} Projects`; // Dynamically set the project title
 
 let selectedIndex = -1;
 
 function renderPieChart(projectsGiven) {
-    let svg = d3.select('.pie-chart-container').select('svg'); 
+    let svg = d3.select('.pie-chart-container').select('svg'); // Ensure the container has a .pie-chart-container class
     if (svg.empty()) {
         svg = d3.select('.pie-chart-container')
             .append('svg')
@@ -18,7 +18,7 @@ function renderPieChart(projectsGiven) {
             .attr('height', 200);
     }
 
-    svg.selectAll('*').remove();
+    svg.selectAll('*').remove(); // Remove previous SVG elements
 
     const legend = d3.select('.legend');
     legend.html('');
@@ -35,7 +35,7 @@ function renderPieChart(projectsGiven) {
     let arcData = sliceGenerator(data);
 
     let arcGenerator = d3.arc().innerRadius(0).outerRadius(80);
-    let colors = d3.scaleOrdinal(d3.schemeTableau10);
+    let colors = d3.scaleOrdinal(d3.schemeTableau10); // You can also try d3.schemeCategory20 if you need more colors
 
     const g = svg.append('g').attr('transform', 'translate(100, 100)');
 
@@ -44,7 +44,7 @@ function renderPieChart(projectsGiven) {
         .enter()
         .append('path')
         .attr('d', arcGenerator)
-        .attr('fill', (_, i) => colors(i))
+        .attr('fill', (_, i) => colors(i)) // Use colors(i) to ensure each slice gets a different color
         .attr('class', (_, i) => i === selectedIndex ? 'selected' : '')
         .on('click', (_, i) => {
             selectedIndex = selectedIndex === i ? -1 : i;
@@ -70,9 +70,8 @@ function renderPieChart(projectsGiven) {
         .attr('style', (_, idx) => `--color:${colors(idx)}`)
         .html((d) => `<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
 
-    projectsTitle.textContent = `${projects.length} Projects`;
+    projectTitle.innerHTML = `${projects.length} Projects`; // Reupdate the title here if needed
     renderProjects(projects, projectsContainer, 'h2');
 }
 
-renderProjects(projects, projectsContainer, 'h2');
 renderPieChart(projects);
